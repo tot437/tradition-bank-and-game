285e34c3c685706b7711ab496171b9b8c7b6b567
TOT <sosomakerone@gmail.com>
2024-10-28 16:36:11 +0300

    new commit line

---
+++ bank form code/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace local_pank_acount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAcount myAcount = new BankAcount("Radwa Banck","phaletien bank","phaletien,gaza,nourth gaza","Ef124358","shecel",1800);
+            BankAcount myAcount1 = new BankAcount();
+            myAcount1.BankName = "elctronic RADWA Bank";
+            myAcount1.BranchName = "egypt branch";
+            myAcount1.BranchAdress = "eygpt, assut, new assut city";
+            myAcount1.AcountNumber = "Ab010048521";
+            myAcount1.AcountCurrency = "L.E";
+            myAcount1.Balance = 1500;
+            Console.WriteLine(myAcount.BankName);
+            Console.WriteLine(myAcount.Balance );
+            Console.WriteLine(myAcount1.Balance);
+            myAcount.WithDraw(200);
+            myAcount.Deposit(250);
+
+            Console.WriteLine();
+            Console.ReadLine();
+            Console.ReadKey();
+        }
+    }
+
+    class BankAcount
+    {
+        public string BankName { get; set; }
+        public string BranchName { get; set; }
+        public string BranchAdress { get; set; }
+        public string AcountNumber { get; set; }
+        public string AcountCurrency { get; set; }
+        public decimal Balance { get; set; }
+
+        public BankAcount()
+        {
+            Console.WriteLine("constructor is called automatically ");
+        }
+        public BankAcount(string BankName, string BranchName, string BranchAdress, string AcountNumber, string AcountCurrency, decimal Balance)
+        {
+            BankName = BankName;
+            BranchName = BranchName;
+            BranchAdress = BranchAdress;
+            AcountNumber = AcountNumber;
+            AcountCurrency = AcountCurrency;
+            Balance = Balance;
+        }
+
+        public void WithDraw(decimal Amount)
+        {
+            decimal newBalance = Balance - Amount;
+            if (newBalance <0 )
+            {
+                Console.WriteLine("Invalid transaction ");
+            }
+            else
+            {
+                Console.WriteLine($"withdrow amount is {Amount}");
+                Console.WriteLine($"Your new balance for acount {AcountNumber} is {newBalance} ");
+            }
+            Console.WriteLine($"Transaction Date {DateTime.Now}");
+        }
+
+        public void Deposit(decimal Amount)
+        {
+            decimal newBalance = Balance + Amount;
+            Console.WriteLine($"Depoit amount is {Amount}");
+            Console.WriteLine($"Your new balance for acount {AcountNumber} is {newBalance} ");
+            Console.WriteLine($"Transaction Date {DateTime.Now}");
+        }
+
+        public void Transfer()
+        {
+
+        }
+
+        public void ChangePassword()
+        {
+
+        }
+
+        public void CreatSubAcount()
+        {
+
+        }
+
+        public void PrintStatement()
+        {
+
+        }
+    }
+}
---
+++ first local game with c#/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace local_game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Enemy1 zombie = new Enemy1();
+            Enemy2 hero = new Enemy2("RODE",200);
+          
+
+            Console.WriteLine(zombie.EnemyName);
+            Console.WriteLine(zombie.Health);
+            Console.WriteLine(hero.EnemyName);
+            Console.WriteLine(hero.Health);
+            
+            
+
+            Console.ReadLine();
+            Console.ReadKey();
+        }
+    }
+
+    class Enemy1
+    {
+        public int Id { get; set; }
+        public string EnemyName { get; set; }
+        public double Health { get; set; }
+
+        public Enemy1()
+        {
+            Console.WriteLine("New Enemy is Created. ");
+            EnemyName = "shapah";
+            Health = 100;
+        }
+    }
+
+    class Enemy2
+    {
+        public int Id { get; set; }
+        public string EnemyName { get; set; }
+        public double Health { get; set; }
+
+        public Enemy2(string enemyName, double health)
+        {
+            Console.WriteLine("New Enemy is Created from parameterized constructor. ");
+            EnemyName = enemyName;
+            Health = health;
+        }
+
+        
+    }
+}
